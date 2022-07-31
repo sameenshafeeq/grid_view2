6c1e3af15d63f119a652f833691c4cfad7460448
sameenshafeeq <sameenshafeeq123@gmail.com>
2022-07-31 16:36:55 -0400

    Add files via upload

--- WebForm1.aspx
+++ WebForm1.aspx
@@ -1,19 +1,19 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Grid_View_Task02.WebForm1" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="task02_state_mangemnt.WebForm1" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title>Grid View :</title>
+    <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
+       <div>
       <asp:TextBox ID="search_box" runat="server" placeholder="Enter the name of the Staff to search!!"></asp:TextBox>
       <asp:Button runat="server" ID="search_button" Text="Seach" onClick="search_button_Click1"/><br />
       <asp:GridView ID="STAFFGRIDDATA" AutoGenerateColumns="false" runat="server" BackColor="White" BorderColor="#E7E7FF"
-      BorderStyle="None" BorderWidth="1px" CellPadding="3" Font-Names="Calibri" Font-Size="Larger" OnRowDeleting="GV_RowDeleting" OnRowEditing="GV_RowEditing"
->
+      BorderStyle="None" BorderWidth="1px" CellPadding="3" Font-Names="Calibri" Font-Size="Larger" OnRowDeleting="GV_RowDeleting" OnRowEditing="GV_RowEditing" OnRowCancelingEdit="TaskGridView_RowCancelingEdit" OnRowUpdating="STAFFGRIDDATA_RowUpdating"
+               AllowPaging="true" OnPageIndexChanging="STAFFGRIDDATA_PageIndexChanging">
           <AlternatingRowStyle BackColor="#F7F7F7" />
                 <FooterStyle BackColor="#B5C7DE" ForeColor="#4A3C8C" />
                 <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
@@ -28,21 +28,9 @@
               <asp:BoundField  DataField="StaffID" HeaderText="ID" />
 	          <asp:BoundField  DataField="StaffName" HeaderText="Name" />
               <asp:BoundField  DataField="StaffRole" HeaderText="Job Role" />
-               <asp:TemplateField>  
-                    <ItemTemplate>  
-                        <asp:Button ID="btn_Edit" runat="server" Text="Edit" CommandName="Edit" />  
-                    </ItemTemplate>  
-                    <EditItemTemplate>  
-                        <asp:Button ID="btn_Update" runat="server" Text="Update" CommandName="Update"/>  
-                        <asp:Button ID="btn_Cancel" runat="server" Text="Cancel" CommandName="Cancel"/>  
-                    </EditItemTemplate>  
-                </asp:TemplateField>  
-
-              <asp:TemplateField>
-                  <ItemTemplate>
-                      <asp:Button ID="btn_update" runat="server" Text="delete" CommandName="Delete" /> 
-                  </ItemTemplate>
-              </asp:TemplateField>
+            
+              <asp:CommandField ShowEditButton="true" />  
+              <asp:CommandField ShowDeleteButton="true" />
           </Columns>
            </asp:GridView> 
         <asp:Label ID="Output_label" runat="server" Text=""></asp:Label>
--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -1,20 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Grid_View_Task02
+namespace task02_state_mangemnt
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Session["data_list"] = 
-           // var my_list = new Staff().GetStaffList();
-            //Session["length_mylist"] = my_list.Count;
-           // Session["length_of_my_array"] = Convert.ToInt32(new Staff().GetStaffList().Count);
             if (!IsPostBack)
             {
                 if (Session["data_list"] == null)
@@ -28,10 +25,6 @@ namespace Grid_View_Task02
                 STAFFGRIDDATA.DataSource = Session["data_list"];
                 STAFFGRIDDATA.DataBind();
             }
-            //Update_List();
-            //STAFFGRIDDATA.DataSource = Session["data_list"];
-            //STAFFGRIDDATA.DataBind();
-;            ///here i am going to store my_list of staff in a secsion_state so that the value can later on be acheived 
         }
         protected void GV_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
@@ -44,29 +37,55 @@ namespace Grid_View_Task02
         }
 
 
-        protected void GV_RowEditing(object sender, GridViewDeleteEventArgs e)
+        protected void GV_RowEditing(object sender, GridViewEditEventArgs e)
         {
-            int indexrow = e.RowIndex;
-            List<Staff> list = (List<Staff>)Session["data_list"];
-            Session["data_list"] = list;
-            STAFFGRIDDATA.DataSource = Session["data_list"];
-            STAFFGRIDDATA.DataBind();
+            STAFFGRIDDATA.EditIndex = e.NewEditIndex;
+            Purani_return_krdoo();
+            //List<Staff> list = (List<Staff>)Session["data_list"];
+        }
+        protected void TaskGridView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            //Reset the edit index.
+            // TaskGridView.EditIndex = -1;
+            //Bind data to the GridView control.
+            STAFFGRIDDATA.EditIndex = -1;
+            Purani_return_krdoo();
 
         }
+        protected void STAFFGRIDDATA_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            //RETERIVING DATA FROM THE SCESSION TABLE;
+            List<Staff> list = (List<Staff>)Session["data_list"];
+            int userid= e.RowIndex;
+            GridViewRow row = (GridViewRow)STAFFGRIDDATA.Rows[userid];
+            //getting the value of the rows based on that specefied index
+            TextBox textName = (TextBox)row.Cells[1].Controls[0];
+            TextBox textRole = (TextBox)row.Cells[2].Controls[0];
+        
+            list[userid].StaffName = textName.Text;
+            list[userid].StaffRole = textRole.Text;
+            
+            STAFFGRIDDATA.EditIndex = -1;
+            Session["data_list"]=list;
+            Purani_return_krdoo();
+        }
+        protected void STAFFGRIDDATA_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            STAFFGRIDDATA.PageIndex = e.NewPageIndex;
+            //Bind data to the GridView control.
+            Purani_return_krdoo();
+        }
+
         protected void Purani_return_krdoo()
         {
-          //  var my_list = new Staff().GetStaffList();
             STAFFGRIDDATA.DataSource = Session["data_list"];
             STAFFGRIDDATA.DataBind();
         }
 
         protected void New_gnerate_krdoo()
         {
-            //STAFFGRIDDATA.DataSource = new Staff().GetStaffList();
-            //STAFFGRIDDATA.DataBind();
-            //var list = Session["data_list"] as List<Staff>;
+            //here updating the index of the array with the randomized key generated
             var list = new Staff().GetStaffList();
-            int number = Convert.ToInt32(Session["length_mylist"]);
             Random rnd = new Random();
             foreach (var elemnt in list)
             {
@@ -78,24 +97,8 @@ namespace Grid_View_Task02
             STAFFGRIDDATA.DataBind();
             Session["data_list"] = list;
         }
-        protected void Update_List()
+        public class Staff
         {
-            //STAFFGRIDDATA.DataSource = new Staff().GetStaffList();
-            //STAFFGRIDDATA.DataBind();
-             //var list = Session["data_list"] as List<Staff>;
-            var list = new Staff().GetStaffList(); 
-            int number=Convert.ToInt32(Session["length_mylist"]);
-            Random rnd = new Random();
-            foreach(var elemnt in list)
-             {
-                 int temp= rnd.Next(10, 99);
-                 string con = (elemnt.StaffID).ToString() + temp.ToString();
-                 elemnt.StaffID = Convert.ToInt32(con);
-             }
-             Session["data_list"] = list;
-        }
-         public class Staff
-         {
             public int StaffID { get; set; }
             public string StaffName { get; set; }
             public string StaffRole { get; set; }
@@ -105,173 +108,194 @@ namespace Grid_View_Task02
             {
                 List<Staff> lstStaff = new List<Staff>();
                 //Here setting the values and adding them in our list..
-                Staff staff = new Staff();
-                staff.StaffID = 1;
-                staff.StaffName = "Talha Shafeeq";
-                staff.StaffRole = "Manager";
-                staff.StaffSalary = "15000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
+
+                Staff staff = new Staff
+                {
+                    StaffID = 1,
+                    StaffName = "Talha Shafeeq",
+                    StaffRole = "Manager",
+                    StaffSalary = "15000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
                 lstStaff.Add(staff);
 
-                staff = new Staff();
-                staff.StaffID = 2;
-                staff.StaffName = "Tuba";
-                staff.StaffRole = "Assiatant";
-                staff.StaffSalary = "12000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
-                lstStaff.Add(staff);
-
-
-                staff = new Staff();
-                staff.StaffID = 3;
-                staff.StaffName = "Tahira";
-                staff.StaffRole = "Manager";
-                staff.StaffSalary = "12000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
+                staff = new Staff
+                {
+                    StaffID = 2,
+                    StaffName = "Tuba",
+                    StaffRole = "Assiatant",
+                    StaffSalary = "12000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
                 lstStaff.Add(staff);
 
 
-
-                staff = new Staff();
-                staff.StaffID = 4;
-                staff.StaffName = "lala";
-                staff.StaffRole = "Worker";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
+                staff = new Staff
+                {
+                    StaffID = 3,
+                    StaffName = "Tahira",
+                    StaffRole = "Manager",
+                    StaffSalary = "12000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
                 lstStaff.Add(staff);
 
 
-                staff = new Staff();
-                staff.StaffID = 5;
-                staff.StaffName = "Hadeeqa";
-                staff.StaffRole = "Worker";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
-                lstStaff.Add(staff);
 
-                staff = new Staff();
-                staff.StaffID = 6;
-                staff.StaffName = "Rana";
-                staff.StaffRole = "Sweeper";
-                staff.StaffSalary = "1000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
-                lstStaff.Add(staff);
-
-                staff = new Staff();
-                staff.StaffID = 7;
-                staff.StaffName = "Majid";
-                staff.StaffRole = "Programmer";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
-                lstStaff.Add(staff);
-
-                staff = new Staff();
-                staff.StaffID = 8;
-                staff.StaffName = "Kamran";
-                staff.StaffRole = "Programmer";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
-                lstStaff.Add(staff);
-
-                staff = new Staff();
-                staff.StaffID = 9;
-                staff.StaffName = "Azra";
-                staff.StaffRole = "Programmer";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
-                lstStaff.Add(staff);
-
-                staff = new Staff();
-                staff.StaffID = 10;
-                staff.StaffName = "Ruhma";
-                staff.StaffRole = "Worker";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
-                lstStaff.Add(staff);
-
-                staff = new Staff();
-                staff.StaffID = 11;
-                staff.StaffName = "Nasir";
-                staff.StaffRole = "Worker";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
+                staff = new Staff
+                {
+                    StaffID = 4,
+                    StaffName = "lala",
+                    StaffRole = "Worker",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
                 lstStaff.Add(staff);
 
 
-                staff = new Staff();
-                staff.StaffID = 12;
-                staff.StaffName = "Waqar";
-                staff.StaffRole = "Worker";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
+                staff = new Staff
+                {
+                    StaffID = 5,
+                    StaffName = "Hadeeqa",
+                    StaffRole = "Worker",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
                 lstStaff.Add(staff);
 
-                staff = new Staff();
-                staff.StaffID = 13;
-                staff.StaffName = "Sameen ul allah";
-                staff.StaffRole = "Worker";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
+                staff = new Staff
+                {
+                    StaffID = 6,
+                    StaffName = "Rana",
+                    StaffRole = "Sweeper",
+                    StaffSalary = "1000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
                 lstStaff.Add(staff);
 
-                staff = new Staff();
-                staff.StaffID = 14;
-                staff.StaffName = "Usama";
-                staff.StaffRole = "Worker";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
+                staff = new Staff
+                {
+                    StaffID = 7,
+                    StaffName = "Majid",
+                    StaffRole = "Programmer",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
+                lstStaff.Add(staff);
+
+                staff = new Staff
+                {
+                    StaffID = 8,
+                    StaffName = "Kamran",
+                    StaffRole = "Programmer",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
+                lstStaff.Add(staff);
+
+                staff = new Staff
+                {
+                    StaffID = 9,
+                    StaffName = "Azra",
+                    StaffRole = "Programmer",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
+                lstStaff.Add(staff);
+
+                staff = new Staff
+                {
+                    StaffID = 10,
+                    StaffName = "Ruhma",
+                    StaffRole = "Worker",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
+                lstStaff.Add(staff);
+
+                staff = new Staff
+                {
+                    StaffID = 11,
+                    StaffName = "Nasir",
+                    StaffRole = "Worker",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
                 lstStaff.Add(staff);
 
 
-                staff = new Staff();
-                staff.StaffID = 15;
-                staff.StaffName = "Mukhtaar";
-                staff.StaffRole = "Worker";
-                staff.StaffSalary = "11000";
-                staff.StaffAddress = "123 Xyz City,Lahore";
+                staff = new Staff
+                {
+                    StaffID = 12,
+                    StaffName = "Waqar",
+                    StaffRole = "Worker",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
+                lstStaff.Add(staff);
+
+                staff = new Staff
+                {
+                    StaffID = 13,
+                    StaffName = "Sameen ul allah",
+                    StaffRole = "Worker",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
+                lstStaff.Add(staff);
+
+                staff = new Staff
+                {
+                    StaffID = 14,
+                    StaffName = "Usama",
+                    StaffRole = "Worker",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
+                lstStaff.Add(staff);
+
+
+                staff = new Staff
+                {
+                    StaffID = 15,
+                    StaffName = "Mukhtaar",
+                    StaffRole = "Worker",
+                    StaffSalary = "11000",
+                    StaffAddress = "123 Xyz City,Lahore"
+                };
                 lstStaff.Add(staff);
                 return lstStaff;
 
             }
-         }
+        }
+        protected void search_button_Click1(object sender, EventArgs e)
+        {
+            var data_to_be_compared = search_box.Text;
+            var list = Session["data_list"] as List<Staff>;
 
-         protected void search_button_Click()
-         {
-             var data_to_be_compared = search_box.Text.ToLower();
-             var list = Session["data_list"] as List<Staff>;
-             List <Staff> output_array= new List<Staff>();
-             foreach(var elemnt in  list)
-             {
-                 if (elemnt.StaffName==data_to_be_compared)
-                 {
-                     Output_label.Text = elemnt.StaffName;
-                 }
-             }
-         }
+            foreach (var elemnt in list)
+            {
+                //if (elemnt.StaffName.ToLower().StartsWith(data_to_be_compared.ToLower()))
+                if(data_to_be_compared.ToLower().Equals(elemnt.StaffName.ToLower()))
+                {
+                    Session["StaffName"] = elemnt.StaffName;
+                    Session["StaffId"] = elemnt.StaffID;
+                    Session["StaffSalary"] = elemnt.StaffSalary;
+                    Session["StaffRole"] = elemnt.StaffRole;
+                    Session["StaffAddress"] = elemnt.StaffAddress;
+                    Response.Redirect("detail.aspx");
+                    break;
+                }
+                else
+                {
+                    Output_label.Text = "Search value is not found!";
+                }
+            }
+        }
 
-         protected void search_button_Click1(object sender, EventArgs e)
-         {
-             var data_to_be_compared = search_box.Text;
-             var list = Session["data_list"] as List<Staff>;
-             List<Staff> output_array = new List<Staff>();
-             foreach (var elemnt in list)
-             {
-                 if (elemnt.StaffName.ToLower().StartsWith(data_to_be_compared.ToLower()))
-                 {
-                     Session["StaffName"] = elemnt.StaffName;
-                     Session["StaffId"] = elemnt.StaffID;
-                     Session["StaffSalary"] = elemnt.StaffSalary;
-                     Session["StaffRole"] = elemnt.StaffRole;
-                     Session["StaffAddress"] = elemnt.StaffAddress;
-                     Response.Redirect("detail.aspx");
-                     break;
-                 }
-                 else
-                 {
-                     Output_label.Text = "Search value is not found!";
-                 }
-             }
-         }
+       
     }
 
-    }
+
+}
+  
